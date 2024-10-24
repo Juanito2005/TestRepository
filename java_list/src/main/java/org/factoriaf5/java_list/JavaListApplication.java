00f5acb93d3fad497a9d07541fee84e3c8177c7d
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2024-10-24 18:01:25 +0200

    The first 3 methods of the classroom task created succesfully

--- java_list/src/main/java/org/factoriaf5/java_list/JavaListApplication.java
+++ java_list/src/main/java/org/factoriaf5/java_list/JavaListApplication.java
@@ -1,5 +1,8 @@
 package org.factoriaf5.java_list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,9 @@ public class JavaListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaListApplication.class, args);
+		List<String> H = new ArrayList<>();
+		Listas g = new Listas();
+		H = g.diasSemana();
+		g.largoLista(H);
 	}
-
 }
--- java_list/src/main/java/org/factoriaf5/java_list/Listas.java
+++ java_list/src/main/java/org/factoriaf5/java_list/Listas.java
@@ -1,5 +1,25 @@
 package org.factoriaf5.java_list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Listas {
-    
+    public List<String> diasSemana() {
+        List<String> dias = new ArrayList<String>();
+        dias.add("Lunes");
+        dias.add("Martes");
+        dias.add("Miercoles");
+        dias.add("Jueves");
+        dias.add("Viernes");
+        dias.add("Sabado");
+        dias.add("Domingo");
+        dias.add("Viernes");
+        dias.add("Sabado");
+        dias.add("Domingo");
+        return dias;
+    }
+    public Integer largoLista(List<String> dias) {
+        System.out.println("LARGO: " + dias.size());
+        return dias.size();
+    }
 }
