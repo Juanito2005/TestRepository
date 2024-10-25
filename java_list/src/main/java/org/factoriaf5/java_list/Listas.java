356ca4655cf47e32bb116cf376128390fbea711e
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2024-10-25 21:59:54 +0200

    Todos los test hechos

--- java_list/src/main/java/org/factoriaf5/java_list/Listas.java
+++ java_list/src/main/java/org/factoriaf5/java_list/Listas.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Listas {
+    // Un método que crea la lista de los días de la semana
+    // Y al mismo tiempo un método que retorna los días de la semana
     public List<String> diasSemana() {
-        List<String> dias = new ArrayList<String>();
+        List<String> dias = new ArrayList<>();
         dias.add("Lunes");
         dias.add("Martes");
         dias.add("Miercoles");
@@ -13,13 +15,43 @@ public class Listas {
         dias.add("Viernes");
         dias.add("Sabado");
         dias.add("Domingo");
-        dias.add("Viernes");
-        dias.add("Sabado");
-        dias.add("Domingo");
         return dias;
     }
+    // Un método que retorne el largo de la lista
     public Integer largoLista(List<String> dias) {
         System.out.println("LARGO: " + dias.size());
         return dias.size();
+    }
+    // Un método para eliminar un día de la semana
+    public List<String> eliminarDia(List<String> dias, int indice) {
+        dias.remove(indice);
+        return dias;
+    }
+    // Un método que retorna el día de la semana solicitado
+    // Y al mismo tiempo un método que retorna si el día solicitado existe en la lista
+    public String retornarDia(List<String> dias, String dia) {
+        if (dias.contains(dia)) {
+            return dia;
+        }
+        return "El dia no se encuentra en la lista";
+    }
+    // Un método para ordenar la lista de días por orden alfabético
+    public List<String> ordenarLista(List<String> dias) {
+        for (int i = 0; i < dias.size(); i++) {
+            String menor = dias.get(i);
+            for (int j = i + 1; j < dias.size(); j++) {
+                if (dias.get(j).compareTo(menor) < 0) {
+                    String temp = dias.get(i);
+                    dias.set(i, dias.get(j));
+                    dias.set(j, temp);
+                }
+            }
+        }
+        System.out.println(dias);
+        return dias;
+    }
+    // Un método para vaciar la lista
+    public void vaciarLista(List<String> dias) {
+        dias.clear();
     }
 }
--- java_list/src/test/java/org/factoriaf5/java_list/ListasTest.java
+++ java_list/src/test/java/org/factoriaf5/java_list/ListasTest.java
@@ -1,5 +1,81 @@
 package org.factoriaf5.java_list;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 public class ListasTest {
-    
+    private List<String> dias;
+    private Listas listas;
+    @BeforeEach
+    public void init() {
+        this.listas = new Listas();
+        this.dias = listas.diasSemana();
+    }
+    @Test
+    public void verificarRetorno () {
+        assertInstanceOf(List.class, dias);
+    }
+    @Test
+    public void largoListatest () {
+        assertEquals(7, listas.largoLista(dias));
+        assertInstanceOf(Integer.class, listas.largoLista(dias));
+    }
+    @Test
+    public void eliminarDiatest () {
+        assertEquals(Arrays.asList("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"), listas.eliminarDia(dias, 6));
+    }
+    @Test
+    public void retornarDia_DiaExistente() {
+        String resultado = listas.retornarDia(dias, "Martes");
+        assertEquals("Martes", resultado);
+    }
+    @Test
+    public void retornarDia_DiaNoExistente() {
+        String resultado = listas.retornarDia(dias, "OtroDomingo");
+        assertEquals("El dia no se encuentra en la lista", resultado);
+    }
+    @Test
+    public void listaElementosOrdenados() {
+        List<String> arancha = Arrays.asList("Sabado", "Viernes");
+        List<String> resultado = listas.ordenarLista(arancha);
+        assertEquals(Arrays.asList("Sabado", "Viernes"), resultado);
+    }
+
+    @Test
+    public void testListaDosElementosDesordenados() {
+        List<String> dias2 = Arrays.asList("Miercoles", "Martes");
+        List<String> resultado = listas.ordenarLista(new ArrayList<>(dias2));
+        assertEquals(Arrays.asList( "Martes", "Miercoles"), resultado);
+    }
+
+    @Test
+    public void VerificarSiOrdena() {
+        List<String> resultado = listas.ordenarLista(new ArrayList<>(dias));
+        assertEquals(Arrays.asList("Domingo", "Jueves", "Lunes", "Martes", "Miercoles", "Sabado", "Viernes"), resultado);
+    }
+
+    @Test
+    public void testListaConElementosRepetidos() {
+        List<String> dias1 = Arrays.asList("Lunes", "Martes", "Lunes", "Miércoles");
+        List<String> resultado = listas.ordenarLista(new ArrayList<>(dias1));
+        assertEquals(Arrays.asList("Lunes", "Lunes", "Martes", "Miércoles"), resultado);
+    }
+
+    @Test
+    public void testListaCadenasVacias() {
+        List<String> dias = Arrays.asList("", "Lunes", "Martes", "");
+        List<String> resultado = listas.ordenarLista(new ArrayList<>(dias));
+        assertEquals(Arrays.asList("", "", "Lunes", "Martes"), resultado);
+    }
+    @Test
+    public void vaciarLista_ListaConElementos() {
+        listas.vaciarLista(dias);
+        assertTrue(dias.isEmpty());
+    }
+
 }
